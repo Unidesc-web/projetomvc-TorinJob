7046d97d3cff67f5effe7f4e6454c8ff993b8c5d
TorinJob <167576031+TorinJob@users.noreply.github.com>
2025-07-03 16:35:16 -0300

    Delete HortifrutidoToinjob directory

--- HortifrutidoToinjob/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace HortifrutidoToinjob.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
